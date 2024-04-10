2e47407c068bb68c3da1ab30a88fcf33cc7a61fb
Kshilinaa <ksusha_2605@mail.ru>
2024-04-10 10:38:46 +0300

    Выполнено задание по task2 создание телеграм бота

--- RoadMapBot/Sources/App/configure.swift
+++ RoadMapBot/Sources/App/configure.swift
@@ -3,12 +3,20 @@ import Fluent
 import FluentPostgresDriver
 import Leaf
 import Vapor
+import TelegramVaporBot
 
 // configures your application
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
+    let tgApi: String = "6958712421:AAEUlb2efcOyQ-ZZb1TCoKnbdRjoxATfHvU"
+    TGBot.log.logLevel = app.logger.logLevel
+    let bot: TGBot = .init(app: app, botId: tgApi)
+    await TGBOT.setConnection(try await TGLongPollingConnection(bot: bot))
+    await DefaultBotHandlers.addHandlers(app: app, connection: TGBOT.connection)
+    try await TGBOT.connection.start()
+    
     app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
         port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
